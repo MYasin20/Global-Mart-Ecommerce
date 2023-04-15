3f202de65df8d01ffc579c3c106f27902f9a4920
MYasin <yasin.ego10@gmail.com>
2023-04-16 07:20:41 +0800

    updated card style

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -52,55 +52,64 @@ export default Products;
 
 const ProductsContainer = styled.div`
   display: flex;
-  justify-content: center;
+  /* justify-content: center; */
   flex: 1 1 100%;
   flex-wrap: wrap;
 `
 
 const Card = styled.div`
   width: 240px;
-  border: 1px solid;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
   margin: 10px 8px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  background: rgba(0, 0, 0, 0.025);
 
   :hover {
-    /* box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px; */
-    /* transform: translateY(-2px); */
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    transform: translateY(-1px);
   }
 `
 const CardImg = styled.img`
-  height: 200px;
+  height:200px;
   /* max-height: 300px; */
-  aspect-ratio: 3/2;
-  object-fit: contain;
-  object-position: center;
+  /* aspect-ratio: 3/2; */
+  /* object-fit: contain; */
+  /* object-position: center; */
 `
 
 const CardBody = styled.div`
   padding: 10px;
-  border: 3px solid purple;
   /* display: flex; */
   /* flex-direction: column; */
   /* justify-content: space-between; */
 `
 const CardTitle = styled.h2`
-  font-size: 20px;
+  font-size: 1.25rem;
+  font-weight: 700;
 `
 const CardDescription = styled.p`
-margin: 10px 0;
+  margin: 10px 0;
 `
-const CardPrice = styled(CardDescription)``
-const CardRating = styled(CardDescription)``
+const CardPrice = styled(CardDescription)`
+  font-size: 18px;
+  font-weight: 500;
+  color: orange ;
+`
+const CardRating = styled(CardDescription)`
+  font-weight: 300;
+`
 
 const BtnAddToCart = styled.button`
   color: white;
   font-size: 1.2rem;
-  background: darkviolet;
+  background: rgb(155,140,255);
   cursor: pointer;
   border: none;
-  padding: 5px 8px;
-  width: 200px;
+  padding: 6px 8px;
+  width: 210px;
+  :hover {
+    background: rgb(151,136,247);
+  }
 `
